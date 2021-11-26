45b73005394839af25d5e118716bf43977a44647
rihan.crh <rihan.crh@autonavi.com>
2021-11-26 16:37:10 +0800

    train minor modify

--- swintransformer/data_processing/data.py
+++ swintransformer/data_processing/data.py
@@ -40,8 +40,7 @@ def distorted_bounding_box_crop(image_bytes,
                                 min_object_covered=0.1,
                                 aspect_ratio_range=(0.75, 1.33),
                                 area_range=(0.05, 1.0),
-                                max_attempts=100,
-                                scope=None):
+                                max_attempts=100):
     """Generates cropped_image using one of the bboxes randomly distorted.
     See `tf.image.sample_distorted_bounding_box` for more documentation.
     Args:
@@ -101,8 +100,7 @@ def _decode_and_random_crop(image_bytes, image_size):
         min_object_covered=0.1,
         aspect_ratio_range=(3. / 4, 4. / 3.),
         area_range=(0.08, 1.0),
-        max_attempts=10,
-        scope=None)
+        max_attempts=10)
     original_shape = tf.image.extract_jpeg_shape(image_bytes)
     bad = _at_least_x_are_equal(original_shape, tf.shape(image), 3)
 
