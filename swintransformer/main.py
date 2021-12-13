a83e3afb90ff38e7fb64a08138bd4a1e50f4502c
rihan.crh <rihan.crh@autonavi.com>
2021-12-13 19:38:35 +0800

    dropout

--- swintransformer/main.py
+++ swintransformer/main.py
@@ -42,7 +42,7 @@ def get_model(model_path, model_name="", pretrained=True, is_training=False):
         if not is_training:
             return [SwinTransformer(model_path, model_name, include_top=False, pretrained=pretrained)]
         else:
-            return [SwinTransformer(model_path, model_name, include_top=False, is_training=True, drop_rate=0.1,
+            return [SwinTransformer(model_path, model_name, include_top=False, drop_rate=0.1,
                                     attn_drop_rate=0.1, pretrained=pretrained)]
     else:
         return [mobilenet_v2(model_path, include_top=False),
